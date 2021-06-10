ed71ab9a5b045439e76084525303ae94dce80753
MarieCichon <marie-chn@hotmail.fr>
2021-06-10 16:26:17 +0200

    ça avance

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'lists#index'
+  resources :lists, only: [:create, :new, :index, :show]
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
